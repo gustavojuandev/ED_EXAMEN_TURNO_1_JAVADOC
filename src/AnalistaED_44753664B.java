dad4888bb487be74971dbf004e4b40f7e2173cfc
gustavojuandev <gustavojuandev@gmail.com>
2022-01-17 15:33:49 +0100

    versión final con javadoc DNI 44753664B

--- src/AnalistaED_44753664B.java
+++ src/AnalistaED_44753664B.java
@@ -1,15 +1,30 @@
+/**
+ * Clase Analista para examen de ED 2022
+ * @author gustavojuan 44753664B
+ * @version 10.0
+ */
 
-// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
 public class AnalistaED_44753664B {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**
+     * Constructor de Clase Analista
+     * @param nombreCompleto string con el nombre completo
+     * @param dni string con el dni
+     */
     public AnalistaED_44753664B(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
+    
+    /**
+     * Devuelve Salario del mes actual
+     * @param horasExtra Entero con las horas de mas
+     * @return Salario final con horas extras incluidas
+     */
 
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
@@ -22,7 +37,7 @@ public class AnalistaED_44753664B {
 
     // Este método no lo comentaremos con JavaDoc
     public static void main(String[] args) {
-        AnalistaED_44753664B emp = new AnalistaED_44753664B("RAUL PALAO","44654545X");
+        AnalistaED_44753664B emp = new AnalistaED_44753664B("Gustavo Juan","44753664B");
         int horasExtra = 2;
         System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra."
         );
