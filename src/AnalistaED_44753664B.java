7f3e75dc32521945b9d323b68ab06c45c2615ea2
gustavojuandev <gustavojuandev@gmail.com>
2022-01-17 15:29:15 +0100

    código sin javadoc DNI 44753664B

---
+++ src/AnalistaED_44753664B.java
@@ -0,0 +1,30 @@
+
+// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
+public class AnalistaED_44753664B {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_44753664B(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        AnalistaED_44753664B emp = new AnalistaED_44753664B("RAUL PALAO","44654545X");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra."
+        );
+    }
+}
